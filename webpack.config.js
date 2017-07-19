4e52ebb9b8c3d90a59ed8534009918c50749e018
veeracs <veeracs@gmail.com>
2017-07-18 22:00:05 -0400

    define module specific scss files, use normalize

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,7 @@ module.exports = [
 
     plugins: [
       ...optimizationPlugins,
+
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
       })
